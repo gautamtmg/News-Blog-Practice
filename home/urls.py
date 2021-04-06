d533db019de983667790d7410f59c3c4de763011
unknown <gautam.tamang0000@gmail.com>
2021-04-06 15:54:59 +0545

    almost completed

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home, beauty,fashionAndStyle, detail, contactus
 
 
 urlpatterns = [
-    path('', home, name="home" )
+    path('', home, name="home" ),
+    path('beauty/', beauty, name="beauty"),
+    path('fashion-and-style/', fashionAndStyle, name="fashion-and-style"),
+    path('detail-post/<int:id>/', detail, name="detail-post"),
+    path('contactus/', contactus, name="contactus")
 ]
--- home/views.py
+++ home/views.py
@@ -6,7 +6,7 @@ from .models import News, Slider
 def home(request):
     news = News.objects.all()
     sliders = Slider.objects.all()
-    categories = ['Beauty', 'Fashion and Style', 'Food']
+    categories = ['beauty', 'fashion and Style']
     context = {
         'news': news,
         'sliders': sliders,
@@ -14,3 +14,27 @@ def home(request):
 
     }
     return render(request, 'index.html', context)
+
+
+def beauty(request):
+    news = News.objects.filter(category='beauty')
+
+
+    return render(request, 'beauty.html', {'news': news})
+
+def fashionAndStyle(request):
+    news = News.objects.filter(category='fashion and Style')
+
+    return render(request, 'fashion.html', {'news': news})
+
+
+
+def detail(request, id):
+    news = News.objects.get(pk=id)
+
+    return render(request, 'blog-single.html', { 'news': news, 'test': "testing" })
+
+
+def contactus(request):
+    
+    return render(request, 'contact.html')
